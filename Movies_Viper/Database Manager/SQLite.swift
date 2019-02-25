a1f26e65522d76be9f75202ff9c6aff15a863e19
Alaa CS <alaa.civilsoft@gmail.com>
2019-02-25 13:06:35 +0200

    first commit

---
+++ Movies_Viper/Database Manager/SQLite.swift
@@ -0,0 +1,9 @@
+//
+//  SQLite.swift
+//  Movies_Viper
+//
+//  Created by Mai Hassan on 2/12/19.
+//  Copyright © 2019 Mai Hassan. All rights reserved.
+//
+
+import Foundation
---
+++ Movies_Viper/MoviesListModule/Interactor/MoviesListInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesListInteractor.swift
+//  Movies_Viper
+//
+//  Created by Mai Hassan on 2/11/19.
+//  Copyright © 2019 Mai Hassan. All rights reserved.
+//
+
+import Foundation
